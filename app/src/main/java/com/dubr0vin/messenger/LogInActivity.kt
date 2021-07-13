1dd33cce9a55cb44c8fef3b45e0251ea23c22e0e
danii <daniildubr0vin@outlook.com>
2021-07-13 18:03:52 +0300

    added loginActivity

--- app/src/main/java/com/dubr0vin/messenger/LogInActivity.kt
+++ app/src/main/java/com/dubr0vin/messenger/LogInActivity.kt
@@ -3,9 +3,11 @@ package com.dubr0vin.messenger
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
     }
 }
