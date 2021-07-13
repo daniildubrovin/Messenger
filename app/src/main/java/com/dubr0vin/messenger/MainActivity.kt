54bfab712d57fa27d8bf71a5bb17940d95f20d67
danii <daniildubr0vin@outlook.com>
2021-07-13 12:58:35 +0300

    Initial commit

---
+++ app/src/main/java/com/dubr0vin/messenger/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dubr0vin.messenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
